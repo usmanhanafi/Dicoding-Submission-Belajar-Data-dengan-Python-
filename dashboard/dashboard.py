ebc68f439e44c7f9cc7755bf31e6cfb929416e57
Usmanhanafi <tholush@gmail.com>
2024-10-09 13:43:49 +0700

    update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -16,59 +16,49 @@ def load_data():
         st.error(f"Terjadi kesalahan: {e}")
 
 data = load_data()
-
 product_df = data[['product_id', 'product_category_name']].drop_duplicates() 
 order_items_df = data[['order_id', 'product_id', 'price', 'freight_value']]
+data['order_purchase_timestamp'] = pd.to_datetime(data['order_purchase_timestamp'], errors='coerce')
 
 st.title("Dashboard Analisis Data E-commerce")
 
 st.sidebar.title("Menu")
-selected_state = st.sidebar.multiselect("Pilih Negara Bagian:", options=data['customer_state'].unique(), default=data['customer_state'].unique())
+available_years = data['order_purchase_timestamp'].dt.year.dropna().astype(int).unique().tolist()
+available_years.sort(reverse=True)  # Sortir dari yang terbaru
 
-product_categories = product_df['product_category_name'].unique().tolist()
-product_categories.insert(0, 'Pilih Semua') 
+selected_year = st.sidebar.selectbox("Pilih Tahun:", available_years)
 
-selected_category = st.sidebar.selectbox("Pilih Kategori Produk:", options=product_categories)
+filtered_data_by_year = data[data['order_purchase_timestamp'].dt.year == selected_year]
 
-revenue_range = st.sidebar.slider("Pilih Rentang Pendapatan:", min_value=0.0, max_value=order_items_df['price'].sum() + order_items_df['freight_value'].sum(), value=(0.0, order_items_df['price'].sum() + order_items_df['freight_value'].sum()))
+selected_state = st.sidebar.multiselect(
+    "Pilih Negara Bagian:", 
+    options=data['customer_state'].dropna().unique(),
+    default=data['customer_state'].dropna().unique()
+)
 
 filtered_data = data[data['customer_state'].isin(selected_state)]
 
-if selected_category == 'Pilih Semua':
-    filtered_order_items = order_items_df[(order_items_df['price'] + order_items_df['freight_value'] >= revenue_range[0]) & (order_items_df['price'] + order_items_df['freight_value'] <= revenue_range[1])]
-else:
-    filtered_order_items = order_items_df[(order_items_df['product_id'].isin(product_df[product_df['product_category_name'] == selected_category]['product_id'])) & (order_items_df['price'] + order_items_df['freight_value'] >= revenue_range[0]) & (order_items_df['price'] + order_items_df['freight_value'] <= revenue_range[1])]
+# Analisis jumlah transaksi per bulan
+monthly_transactions = filtered_data_by_year.groupby(filtered_data_by_year['order_purchase_timestamp'].dt.month).size()
+monthly_transactions = monthly_transactions.reindex(range(1, 13), fill_value=0)
 
-st.subheader("Total Transaksi dalam Bulan Terakhir")
+# Menampilkan total transaksi
+total_transaksi = filtered_data_by_year['order_id'].nunique()
+st.subheader(f"Total Transaksi pada Tahun {selected_year} : {total_transaksi}")
 
-total_transactions = filtered_order_items['order_id'].nunique()
-st.write(f"Total transaksi: {total_transactions}")
+bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
 
+# Visualisasi data total transaksi perbulan pada setiap tahunnya(tahun terakhir)
+fig, ax = plt.subplots()
+sns.lineplot(x=monthly_transactions.index, y=monthly_transactions.values, marker='o', ax=ax)
+ax.set_xticks(range(1, 13))
+ax.set_xticklabels(bulan, rotation=45)
+ax.set_title(f'Jumlah Transaksi per Bulan pada Tahun {selected_year}')
+ax.set_xlabel('Bulan')
+ax.set_ylabel('Jumlah Transaksi')
+
+# Visualisasi data distribusi pelanggan berdasarkan negara bagian
+st.pyplot(fig)
 st.subheader("Distribusi Pelanggan Berdasarkan Negara Bagian")
 state_counts = filtered_data['customer_state'].value_counts()
 st.bar_chart(state_counts)
-
-st.subheader("Pendapatan Tertinggi per Produk")
-
-filtered_order_items['total_revenue'] = filtered_order_items['price'] + filtered_order_items['freight_value'] 
-revenue_per_product = filtered_order_items.groupby('product_id')['total_revenue'].sum().reset_index()
-
-merged_data = pd.merge(revenue_per_product, product_df, on='product_id', how='left')
-
-def format_currency(value):
-    return f"${value:,.2f}" 
-
-merged_data['formatted_revenue'] = merged_data['total_revenue'].apply(format_currency)
-
-plt.figure(figsize=(10, 6))
-top_products = merged_data.sort_values('total_revenue', ascending=False).head(10)
-
-sns.barplot(x='total_revenue', y='product_category_name', data=top_products, palette='viridis')
-plt.title('Produk dengan Pendapatan Tertinggi')
-plt.xlabel('Total Pendapatan (dollar)')
-plt.ylabel('Nama Kategori Produk') 
-st.pyplot(plt)
-
-
-if __name__ == "__main__":
-    st.sidebar.write("Selamat datang di dashboard analisis data e-commerce!")
